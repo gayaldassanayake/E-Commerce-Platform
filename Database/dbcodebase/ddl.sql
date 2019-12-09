6aa8edd415739e71c77e55d32c370ee92bea991d
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2019-12-09 15:27:04 +0530

    Database ddl statements updated with new changes after mid evaluation

--- Database/dbcodebase/ddl.sql
+++ Database/dbcodebase/ddl.sql
@@ -37,7 +37,7 @@ CREATE TABLE credit_card_detail(
     cvv char(3) ,
     expire_year NUMERIC(4, 0),
     expire_month numeric(2, 0),
-    name_on_card varchar(200) not null,
+    name_on_card varchar(200),
     primary key (customer_id, credit_card_number),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -45,7 +45,10 @@ CREATE TABLE credit_card_detail(
 CREATE TABLE product(
     product_id char(6),
     title varchar(100) not null,
+    description text not null,
     manufacturer varchar(50) NOT NULL,
+    state char(30) not null,
+    rating int,
     PRIMARY KEY(product_id)
 );
 
@@ -53,10 +56,12 @@ CREATE TABLE varient(
     product_id char(6),
     varient_id char(6),
     sku char(10) NOT NULL unique,
+    title char(100) NOT NULL,
     price numeric(12, 2) NOT NULL,
-    quantity int ,
-    weight numeric(6, 2) ,
-    restock_limit int ,
+    quantity int not null,
+    state char(30) not null,
+    weight numeric(6, 2),
+    restock_limit int not null,
     image_path varchar(255) NOT NULL,
     PRIMARY KEY(product_id, varient_id),
     FOREIGN KEY (product_id) REFERENCES product(product_id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -64,32 +69,50 @@ CREATE TABLE varient(
 
 CREATE TABLE `order`(
     order_id char(7),
-    state char(30),
-    delivery_method char(20),
-    payment_method char(25),
+    state char(30) not null,
+    delivery_method char(20) not null,
+    payment_method char(25) not null,
     credit_card_number char(16),
     cvv char(3),
     expire_year NUMERIC(4, 0),
     expire_month numeric(2, 0),
-    name_on_card varchar(200) not null,
-    PRIMARY KEY (order_id)
+    name_on_card varchar(200),
+    delivery_address varchar(255),
+    estimate_days numeric(2, 0),
+    delivery_person_id char(5),
+    
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (delivery_person_id) REFERENCES delivery_person(delivery_person_id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE varient_item(
     product_id char(6),
     varient_id char(6),
     serial_number char(10),
-    order_id char(7),
+    
     availability boolean NOT NULL,
     PRIMARY KEY(product_id, varient_id, serial_number),
-    FOREIGN KEY(order_id) REFERENCES `order`(order_id) ON UPDATE CASCADE ON DELETE RESTRICT,
     FOREIGN KEY (product_id, varient_id) REFERENCES varient(product_id, varient_id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE order_item(
+    product_id char(6),
+    varient_id char(6),
+    serial_number char(10),
+    order_id char(6),
+    price numeric(12,2) not null,
+
+    PRIMARY KEY(product_id, varient_id, serial_number,order_id),
+    FOREIGN KEY (product_id, varient_id,serial_number) REFERENCES varient_item(product_id, varient_id,serial_number) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (order_id) REFERENCES `order`(order_id) ON UPDATE CASCADE ON DELETE RESTRICT
+
 );
 
 CREATE TABLE category(
     category_id char(6),
-    category varchar(50),
+    category varchar(50) not null,
     super_category_id char(6),
+    state char(30) not null,
     PRIMARY KEY(category_id),
     FOREIGN KEY(super_category_id) REFERENCES category(category_id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
@@ -134,18 +157,27 @@ CREATE TABLE shopping_cart_item(
     customer_id char(7),
     product_id char(6),
     varient_id char(6),
-    quantity int,
+    quantity int not null,
     PRIMARY KEY(customer_id, product_id, varient_id),
     FOREIGN KEY(customer_id) REFERENCES customer(customer_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY(product_id, varient_id) REFERENCES varient(product_id, varient_id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
-CREATE TABLE delivery_detail(
-    order_id char(7),
-    delivery_person_id char(5),
-    address varchar(255) not null,
-    estimate_days numeric(2, 0) ,
-    PRIMARY KEY (order_id, delivery_person_id),
-    FOREIGN KEY (order_id) REFERENCES `order`(order_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (delivery_person_id) REFERENCES delivery_person(delivery_person_id) ON UPDATE CASCADE ON DELETE RESTRICT
+CREATE TABLE comment(
+    comment_id int AUTO_INCREMENT,
+    customer_id char(7),
+    product_id char(6),
+    comment text not null,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(product_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE rating(
+    customer_id char(7),
+    product_id char(6),
+    rating int,
+    PRIMARY KEY (customer_id,product_id),
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(product_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
