468a2cc0d686b8dd15b97d0adb9be1cf65359009
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2019-12-12 00:18:22 +0530

    Cart page connection halfway

--- app/routes/home.js
+++ app/routes/home.js
@@ -9,5 +9,6 @@ const router = express.Router();
 
 router.get('/', customerController.indexAction);
 router.get('/test',testController.testAction);
+router.get('/cart',customerController.cartAction)
 
 module.exports = router;
