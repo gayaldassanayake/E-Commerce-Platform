ea0638bd93f369d3049614a8748919518e6e2da8
Achinths Isuru <achinthaisuru444@gmail.com>
2019-12-12 13:11:17 +0530

    started the ordermodel.js

---
+++ app/models/orderModel.js
@@ -0,0 +1,33 @@
+const db = require('../utils/database');
+
+module.exports = class Order {
+    constructor(order_id, state, delivery_method, payment_method, credit_card_number,cvv,expire_month,expire_year,name_on_card,delivery_address,estimate_days,delivery_person_id) {
+        this.order_id = order_id;
+        this.delivery_method = delivery_method;
+        this.payment_method = payment_method;
+        this.credit_card_number = credit_card_number;
+        this.cvv = cvv;
+        this.expire_month = expire_month;
+        this.expire_year = expire_year;
+        this.name_on_card = name_on_card;
+        this.delivery_address = delivery_address;
+        this.estimate_days = estimate_days;
+        this.delivery_person_id = delivery_person_id;
+        this.state = state;
+    }
+
+    save() {
+       db.execute('INSERT INTO products (title, description, manufacturer, state, rating) VALUES (?,?,?,?,?,?)',
+       [this.title, this.description, manufacturer, this.state, this.rating]
+       );
+    }
+
+    static fetchAll() {
+        db.execute("SELECT * FROM product").then((res)=>{
+            console.log(res);
+        }).catch((err)=> {
+            console.log(err);
+        });
+    }
+};
+
