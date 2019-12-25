81447fe7422c976fe35174ab305931b22bc69c16
Damika Gamlath <damika97.10@gmail.com>
2019-12-25 16:52:09 +0530

    dtpicker for top sales

--- app/routes/adminRouter.js
+++ app/routes/adminRouter.js
@@ -13,4 +13,6 @@ router.get('/view_product',adminController.view_productAction);
 router.get('/report',adminController.reportAction);
 router.get('/report/top_sold_products/',adminController.top_sold_productsAction);
 
+router.post('/report/top_sold_products/',adminController.top_sold_productsPOSTAction);
+
 module.exports = router;
