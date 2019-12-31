35f91552a44667b9881b6f8539ef0ce91012f343
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2019-12-31 08:17:05 +0530

    quick fix

--- app/utils/errorMessage.js
+++
@@ -1,7 +0,0 @@
-module.exports = (errorCode) => {
-    let errorMessages = {
-        "00001" : "Invalid Username !",
-        "00002" : "Invalid Password !"
-    }
-    return errorMessages[errorCode];
-}
